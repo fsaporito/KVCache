e49b3803139f742e1dfe8b7cc9602769664b8733
fsaporito <sap93@gmail.com>
2022-01-08 13:45:13 +0100

    Add MemoryCacheMap.cpp

---
+++ src/KVCache/MemoryCache/MemoryCacheMap.cpp
@@ -0,0 +1,104 @@
+#include "MemoryCacheMap.h"
+
+#include <spdlog/spdlog.h>
+#include <stdexcept>
+
+using namespace KVCache::Internal::MemoryCache;
+
+template<typename T>
+    requires isMap<T>
+        MemoryCacheMap<T>::MemoryCacheMap(const size_t maxByteSize,
+                                          const Interface::CacheEvictionStrategy cacheEvictionStrategy)
+        : AbstractMemoryCache(maxByteSize, cacheEvictionStrategy)
+    {
+        spdlog::trace("MemoryCacheMap Object Created");
+    };
+
+
+    template<typename T>
+        requires isMap<T>
+            std::vector<std::pair<std::string, std::string>> MemoryCacheMap<T>::put(const std::string& key, const std::string& value)
+        {
+            std::vector<std::pair<std::string, std::string>> removedElements{};
+
+            std::lock_guard<std::mutex> guard(m_mapMutex);
+
+            if (m_map.contains(key))
+            {
+                spdlog::trace("MemoryCacheMap::put() key={} is present, value will be updated", key);
+                const size_t oldValueByteSize = m_map.at(key).size();
+                m_currentByteSize -= oldValueByteSize;
+                m_map[key] = value;
+                const size_t newValueByteSize = value.size();
+                m_currentByteSize += newValueByteSize;
+            }
+            else
+            {
+                spdlog::trace("MemoryCacheMap::put() key={} isn't present, value will be added", key);
+                const auto kvPair = std::make_pair(key, value);
+                m_map.insert(kvPair);
+                const size_t newKeyByteSize = key.size();
+                const size_t newValueByteSize = value.size();
+                m_currentByteSize += newKeyByteSize + newValueByteSize;
+            }
+
+            spdlog::trace("MemoryCacheMap::put() completed for key={}, new bytesize=", key, m_currentByteSize);
+
+            return removedElements;
+        }
+
+    template<typename T>
+        requires isMap<T>
+    void MemoryCacheMap<T>::remove(const std::string& key)
+    {
+        std::lock_guard<std::mutex> guard(m_mapMutex);
+        if (m_map.contains(key))
+        {
+            spdlog::trace("MemoryCacheMap::remove() remove key={}", key);
+            const size_t oldValueByteSize = m_map.at(key).size();
+            m_map.erase(key);
+            m_currentByteSize -= oldValueByteSize;
+            spdlog::trace("MemoryCacheMap::remove() removed key={}, new bytesize=", key, m_currentByteSize);
+        }
+    }
+
+    template<typename T>
+        requires isMap<T>
+            std::pair<std::string, std::string> MemoryCacheMap<T>::get(const std::string& key)
+    const
+    {
+        std::lock_guard<std::mutex> guard(m_mapMutex);
+        if (m_map.contains(key))
+        {
+            spdlog::trace("MemoryCacheMap::get() key={}", key);
+            return std::make_pair(key, m_map.at(key));
+        }
+        else
+        {
+            spdlog::trace("MemoryCacheMap::get() failed for key={}", key);
+            throw std::runtime_error("Didn't find element with key: " + key);
+        }
+    }
+
+    template<typename T>
+        requires isMap<T>
+            size_t MemoryCacheMap<T>::getByteSize()
+    const
+    {
+        std::lock_guard<std::mutex> guard(m_mapMutex);
+        spdlog::trace("MemoryCacheMap::getByteSize() = {}", m_currentByteSize);
+        return m_currentByteSize;
+    }
+
+    template<typename T>
+        requires isMap<T>
+            size_t MemoryCacheMap<T>::size()
+    const
+    {
+        std::lock_guard<std::mutex> guard(m_mapMutex);
+        spdlog::trace("MemoryCacheMap::getByteSize() = {}", m_map.size());
+        return m_map.size();
+    }
+
+    template class KVCache::Internal::MemoryCache::MemoryCacheMap<std::map<std::string, std::string>>;
+    template class KVCache::Internal::MemoryCache::MemoryCacheMap<std::unordered_map<std::string, std::string>>;

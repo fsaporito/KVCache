c39b7826b6113d5c8073803d143e8f757336aec0
fsaporito <sap93@gmail.com>
2022-01-08 13:44:45 +0100

    Move MemoryCacheMap templated methods to .cpp file

--- src/KVCache/MemoryCache/MemoryCacheMap.h
+++ src/KVCache/MemoryCache/MemoryCacheMap.h
@@ -3,8 +3,6 @@
 #include <concepts>
 #include <map>
 #include <mutex>
-#include <spdlog/spdlog.h>
-#include <stdexcept>
 #include <unordered_map>
 
 namespace KVCache
@@ -25,11 +23,7 @@ namespace KVCache
             {
               public:
                 MemoryCacheMap(const size_t maxByteSize,
-                               const Interface::CacheEvictionStrategy cacheEvictionStrategy)
-                    : AbstractMemoryCache(maxByteSize, cacheEvictionStrategy)
-                {
-                    spdlog::trace("MemoryCacheMap Object Created");
-                };
+                               const Interface::CacheEvictionStrategy cacheEvictionStrategy);
 
                 ~MemoryCacheMap() = default;
                 MemoryCacheMap(const MemoryCacheMap&) = delete;
@@ -43,35 +37,7 @@ namespace KVCache
                  * If the KV cache is full, will return a vector containing the evicted pairs.
                  * Eviction Strategy is given at object creation.
                  */
-                std::vector<std::pair<std::string, std::string>> put(const std::string& key, const std::string& value) override
-                {
-                    std::vector<std::pair<std::string, std::string>> removedElements{};
-
-                    std::lock_guard<std::mutex> guard(m_mapMutex);
-
-                    if (m_map.contains(key))
-                    {
-                        spdlog::trace("MemoryCacheMap::put() key={} is present, value will be updated", key);
-                        const size_t oldValueByteSize = m_map.at(key).size();
-                        m_currentByteSize -= oldValueByteSize;
-                        m_map[key] = value;
-                        const size_t newValueByteSize = value.size();
-                        m_currentByteSize += newValueByteSize;
-                    }
-                    else
-                    {
-                        spdlog::trace("MemoryCacheMap::put() key={} isn't present, value will be added", key);
-                        const auto kvPair = std::make_pair(key, value);
-                        m_map.insert(kvPair);
-                        const size_t newKeyByteSize = key.size();
-                        const size_t newValueByteSize = value.size();
-                        m_currentByteSize += newKeyByteSize + newValueByteSize;
-                    }
-
-                    spdlog::trace("MemoryCacheMap::put() completed for key={}, new bytesize=", key, m_currentByteSize);
-
-                    return removedElements;
-                }
+                std::vector<std::pair<std::string, std::string>> put(const std::string& key, const std::string& value) override;
 
                 /*!
                  * Removes KV pair from the cache.
@@ -79,18 +45,7 @@ namespace KVCache
                  * This method will automatically update the bytesize of the KVCache.
                  * This method is thread-safe regarding access to the underline data.
                  */
-                void remove(const std::string& key) override
-                {
-                    std::lock_guard<std::mutex> guard(m_mapMutex);
-                    if (m_map.contains(key))
-                    {
-                        spdlog::trace("MemoryCacheMap::remove() remove key={}", key);
-                        const size_t oldValueByteSize = m_map.at(key).size();
-                        m_map.erase(key);
-                        m_currentByteSize -= oldValueByteSize;
-                        spdlog::trace("MemoryCacheMap::remove() removed key={}, new bytesize=", key, m_currentByteSize);
-                    }
-                }
+                void remove(const std::string& key) override;
 
                 /*!
                  * Get a KV pair from the cache.
@@ -98,42 +53,19 @@ namespace KVCache
                  * This method won't change the underlying data.
                  * This method is thread-safe regarding access to the underline data.
                  */
-                std::pair<std::string, std::string> get(const std::string& key) const override
-                {
-                    std::lock_guard<std::mutex> guard(m_mapMutex);
-                    if (m_map.contains(key))
-                    {
-                        spdlog::trace("MemoryCacheMap::get() key={}", key);
-                        return std::make_pair(key, m_map.at(key));
-                    }
-                    else
-                    {
-                        spdlog::trace("MemoryCacheMap::get() failed for key={}", key);
-                        throw std::runtime_error("Didn't find element with key: " + key);
-                    }
-                }
+                std::pair<std::string, std::string> get(const std::string& key) const override;
 
                 /*!
                  * Returns the bytesize of the KVCache.
                  * This method is thread-safe regarding access to the underline data.
                  */
-                size_t getByteSize() const override
-                {
-                    std::lock_guard<std::mutex> guard(m_mapMutex);
-                    spdlog::trace("MemoryCacheMap::getByteSize() = {}", m_currentByteSize);
-                    return m_currentByteSize;
-                }
+                size_t getByteSize() const override;
 
                 /*!
                  * Returns the number of KVPairs in the KVCache.
                  * This method is thread-safe regarding access to the underline data.
                  */
-                size_t size() const override
-                {
-                    std::lock_guard<std::mutex> guard(m_mapMutex);
-                    spdlog::trace("MemoryCacheMap::getByteSize() = {}", m_map.size());
-                    return m_map.size();
-                }
+                size_t size() const override;
 
               protected:
                 mutable std::mutex m_mapMutex{};

c307880c7ccfdb2bfdb703bb95e6e2f3cb4e15e3
fsaporito <sap93@gmail.com>
2022-01-12 18:22:56 +0100

    Fix release typo

--- scripts/cmake_full.sh
+++ scripts/cmake_full.sh
@@ -7,7 +7,7 @@ then
     exit
 fi
 cd ..
-cmake -S . -B ./build/release -DCMAKE_BUILD_TYPE:STRING=Relese
+cmake -S . -B ./build/release -DCMAKE_BUILD_TYPE:STRING=Release
 cmake -S . -B ./build/debug -DCMAKE_BUILD_TYPE:STRING=Debug -DENABLE_COVERAGE:BOOL=$DENABLE_COVERAGE:BOOL=TRUE
 cmake -S . -B ./build/minSizeRel -DCMAKE_BUILD_TYPE:STRING=MinSizeRel
 cmake -S . -B ./build/relWithDebInfo -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo

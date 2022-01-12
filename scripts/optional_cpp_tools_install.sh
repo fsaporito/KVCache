4c21e0446d61e0da7ba2cc59e5abb4fb3f1acbe9
fsaporito <sap93@gmail.com>
2022-01-12 13:29:26 +0100

    Split cpp tools install script into required and optional

---
+++ scripts/optional_cpp_tools_install.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# Doxygen
+sudo apt-get install doxygen
+sudo apt-get install graphviz
+
+# CCache
+sudo apt-get install ccache
+
+# CppCheck
+sudo apt-get install cppcheck
+sudo apt-get install cppcheck-gui
---
+++ scripts/required_cpp_tools_install.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# Build Esstentials
+sudo apt install build-essential make clang
+
+# CMake
+sudo apt-get install cmake
+sudo apt-get install cmake-gui
